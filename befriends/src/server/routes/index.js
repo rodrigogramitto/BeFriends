db2cc995cbd68753461572a4dd349e6739cec574
rodrigogramitto <rgramitto@gmail.com>
2023-06-09 16:09:02 -0400

    Modified routes to Ecma6

--- befriends/src/server/routes/index.js
+++ befriends/src/server/routes/index.js
@@ -1,18 +1,21 @@
-var router = require('express').Router();
-const { requiresAuth } = require('express-openid-connect');
 
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Auth0 Webapp sample Nodejs',
-    isAuthenticated: req.oidc.isAuthenticated()
-  });
+import express from 'express';
+import { requiresAuth } from 'express-openid-connect';
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+res.render('index', {
+title: 'Auth0 Webapp sample Nodejs',
+isAuthenticated: req.oidc.isAuthenticated()
+});
 });
 
-router.get('/profile', requiresAuth(), function (req, res, next) {
-  res.render('profile', {
-    userProfile: JSON.stringify(req.oidc.user, null, 2),
-    title: 'Profile page'
-  });
+router.get('/profile', requiresAuth(), (req, res, next) => {
+res.render('profile', {
+userProfile: JSON.stringify(req.oidc.user, null, 2),
+title: 'Profile page'
+});
 });
 
-module.exports = router;
+export default router;
