1d3efd567dc0f2745d4d2ee44e23ec8437eb3a0f
rodrigogramitto <rgramitto@gmail.com>
2023-06-09 12:01:06 -0400

    Basic repo setup:wq

---
+++ befriends/src/server/server.js
@@ -0,0 +1,61 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import http from 'http';
+import logger from 'morgan';
+import path from 'path';
+import router from './routes/index';
+import { auth } from 'express-openid-connect';
+
+dotenv.config();
+
+dotenv.load();
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+const config = {
+  authRequired: false,
+  auth0Logout: true
+};
+
+const port = process.env.PORT || 3000;
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+  config.baseURL = `http://localhost:${port}`;
+}
+
+app.use(auth(config));
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
+
+app.use('/', router);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handlers
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: process.env.NODE_ENV !== 'production' ? err : {}
+  });
+});
+
+http.createServer(app)
+  .listen(port, () => {
+    console.log(`Listening on ${config.baseURL}`);
+  });
