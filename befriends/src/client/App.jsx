a80ecc52306021329b4adb88b4a4c6e33227706c
rodrigogramitto <rgramitto@gmail.com>
2023-06-12 14:45:32 -0400

    solve

--- befriends/src/client/App.jsx
+++ befriends/src/client/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './components/loginButton.jsx';
 import LogoutButton from './components/logoutButton.jsx';
-import Profile from './components/Profile/Profile.jsx';
+import Profile from './components/Profile/Profile/jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,13 +10,16 @@ function App() {
 
   return (
     <>
-      <div className='bg-primary'>
-    <h1 className="text-5xl font-bold underline">
+    <div className='bg-primary'>
+      <h1 className="text-5xl font-bold underline">
     Hello world!
     </h1>
+
       {(!isAuthenticated) &&
         <LoginButton />
       }
+
+
       {(isAuthenticated) &&
        <LogoutButton />
       }
@@ -26,5 +29,3 @@ function App() {
 }
 
 export default App
-
-//{ isAuthenticated ? <LogoutButton/> : <LoginButton/> }
