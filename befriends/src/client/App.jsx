b4b5dcadf96088fc23447f5584bd67d82fd8ddb1
Sean Jung <sean.jung23@gmail.com>
2023-06-12 16:59:55 -0400

    basic form functionality

--- befriends/src/client/App.jsx
+++ befriends/src/client/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './components/loginButton.jsx';
 import LogoutButton from './components/logoutButton.jsx';
-import Profile from './components/Profile/Profile/jsx';
+import Questionnaire from './components/Questionnaire.jsx';
+import Profile from './components/Profile/Profile.jsx';
 import NavBar from './components/navBar.jsx'
 
 
@@ -20,7 +21,10 @@ function App() {
 
 
       {(isAuthenticated) &&
-       <LogoutButton />
+      <>
+        <Questionnaire />
+        <LogoutButton />
+      </>
       }
     </>
   )
