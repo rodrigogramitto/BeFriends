596f82054d32b152884f1bd6429dd44db57bdbbd
Neeki Askari <neeki.askari@gmail.com>
2023-06-12 15:43:28 -0400

    daisy and tailwind now working, nav bar added

--- befriends/src/client/App.jsx
+++ befriends/src/client/App.jsx
@@ -4,17 +4,14 @@ import LoginButton from './components/loginButton.jsx';
 import LogoutButton from './components/logoutButton.jsx';
 import NavBar from './components/navBar.jsx'
 
+
 function App() {
   const [count, setCount] = useState(0)
   const { isAuthenticated } = useAuth0()
 
   return (
     <>
-    <div className='bg-primary'> 
       <NavBar />
-      <h1 className="text-5xl font-bold underline">
-    Hello world!
-    </h1>
 
       {(!isAuthenticated) &&
         <LoginButton />
@@ -24,9 +21,8 @@ function App() {
       {(isAuthenticated) &&
        <LogoutButton />
       }
-      </div>
     </>
   )
 }
 
-export default App
+export default App;
