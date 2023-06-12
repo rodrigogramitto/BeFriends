11523f569747124876fd603b4022fb6380342472
Neeki Askari <neeki.askari@gmail.com>
2023-06-12 12:23:32 -0400

    navBar component added to App.jsx and html css file path corrected

--- befriends/src/client/App.jsx
+++ befriends/src/client/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './components/loginButton.jsx';
 import LogoutButton from './components/logoutButton.jsx';
+import NavBar from './components/navBar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <div className='bg-primary'> 
+      <NavBar />
       <h1 className="text-5xl font-bold underline">
     Hello world!
     </h1>
