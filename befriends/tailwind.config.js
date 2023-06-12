a0ba89769c710fbe600a684f5679a1fd39d33fdb
Neeki Askari <neeki.askari@gmail.com>
2023-06-12 17:11:49 -0400

    daisy-ui colors now working

--- befriends/tailwind.config.js
+++ befriends/tailwind.config.js
@@ -1,13 +1,6 @@
 // tailwind.config.js
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: 'var(--color-primary)',
-      },
-    },
-  },
   daisyui: {
     themes: [
       {
