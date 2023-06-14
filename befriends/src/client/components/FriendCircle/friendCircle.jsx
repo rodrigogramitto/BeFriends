0576d9a97a8577b314e054e9c04eca9908b70d5a
Andre Bagur <andre.bagur@gmail.com>
2023-06-14 18:30:06 -0500

    deleted example.env

--- befriends/src/client/components/FriendCircle/friendCircle.jsx
+++ befriends/src/client/components/FriendCircle/friendCircle.jsx
@@ -7,15 +7,14 @@ function FriendCircle ({currentUser}) {
   return (
     <div className="flex justify-center" style={{height: "90%"}}>
         <div className="flex flex-row gap-5" style={{
-            border: "1px solid black",
             height: "80vh",
             width: "90%",
             }}>
             <div className="flex flex-col">
                 <div style={{
                 border: "1px solid blue",
-                height: "400px",
-                width: "400px",
+                height: "600px",
+                width: "600px",
                 overflow: "auto",
                 }}>
                 <MyCalendar/>
