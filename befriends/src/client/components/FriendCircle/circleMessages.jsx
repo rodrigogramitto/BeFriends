2c75d6fbb72a5f1738cfaee2bac2a5b6eec3d1a0
Andre Bagur <andre.bagur@gmail.com>
2023-06-16 10:13:29 -0500

    minor styling changes

--- befriends/src/client/components/FriendCircle/circleMessages.jsx
+++ befriends/src/client/components/FriendCircle/circleMessages.jsx
@@ -5,7 +5,7 @@ function CircleMessages ({userChats, setCurrentChat}) {
     return (
     <div className="flex justify-center">
         <div className="tabs flex flex-col">
-        <a className="tab tab-lg" >Direct Messages</a>
+        <a className="tab tab-bordered tab-lg" >My FriendCircles:</a>
           {(userChats.length > 0 ? userChats.map((chat, index) => {
             return (<a className="tab tab-lg" key={index} onClick={() => {
               setCurrentChat(chat.chatId);
